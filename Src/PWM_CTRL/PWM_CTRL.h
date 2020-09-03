171bf204e1d9eeb01d7b51c75a84c5d099a3e620
Macikow <kow.maciej@gmail.com>
2020-09-04 00:10:39 +0200

    dodanie obsługi liczników PWM

---
+++ Src/PWM_CTRL/PWM_CTRL.h
@@ -0,0 +1,14 @@
+/*
+ * PWM_CTRL.h
+ *
+ *  Created on: 3 wrz 2020
+ *      Author: kowma
+ */
+
+#ifndef PWM_CTRL_PWM_CTRL_H_
+#define PWM_CTRL_PWM_CTRL_H_
+
+void pwmctrl_enable_timer_irq(void);
+
+
+#endif /* PWM_CTRL_PWM_CTRL_H_ */
